e6410b9a62f9eb1f9abe2ddf87cd4c8444871b9b
Felipe Campanaro <felipe.s.campanaro@gmail.com>
2020-05-02 19:33:11 -0300

    Search a user by id

--- src/main/java/com/felipedeveloper/mongodb/services/UserService.java
+++ src/main/java/com/felipedeveloper/mongodb/services/UserService.java
@@ -1,12 +1,14 @@
 package com.felipedeveloper.mongodb.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.felipedeveloper.mongodb.domain.User;
 import com.felipedeveloper.mongodb.repository.UserRepository;
+import com.felipedeveloper.mongodb.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -17,5 +19,10 @@ public class UserService {
 	public List<User> findAll(){
 		return repo.findAll();
 		
+	}
+	
+	public User findById(String id) {
+		Optional<User> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Object not found"));
 	}
 }
