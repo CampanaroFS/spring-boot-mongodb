3b0e6bd2a1f98614312cff17d7fb0615a3bde159
Felipe Campanaro <felipe.s.campanaro@gmail.com>
2020-05-03 17:13:33 -0300

    Insert user by POST

--- src/main/java/com/felipedeveloper/mongodb/WorkshopmongoApplication.java
+++ src/main/java/com/felipedeveloper/mongodb/WorkshopmongoApplication.java
@@ -3,6 +3,8 @@ package com.felipedeveloper.mongodb;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.felipedeveloper.mongodb.domain.User;
+
 @SpringBootApplication
 public class WorkshopmongoApplication {
 
